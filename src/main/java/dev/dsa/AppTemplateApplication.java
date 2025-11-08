ecf1a4938248a658677dabc8fa2be9ddb7684b8c
Deepak <aswal.dsa@gmail.com>
2025-11-08 13:35:13 +0100

    firstcommit

---
+++ src/main/java/dev/dsa/AppTemplateApplication.java
@@ -0,0 +1,13 @@
+package dev.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dsa/AppTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
